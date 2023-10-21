40d1567f31ef96af1658f0b538ca3094a394ed02
atharva <atharvamk6@gmail.com>
2023-10-21 16:31:22 +0530

    AM and PM Feature Added

--- clock.js
+++ clock.js
@@ -1,27 +1,45 @@
-function updateClock() {
-const now = new Date();
-const hourFormat = String(now.getHours()).padStart(2, '0');
-const minutes = String(now.getMinutes()).padStart(2, '0');
-const seconds = String(now.getSeconds()).padStart(2, '0');
-
+const aM = document.getElementById('AM') 
+const pM = document.getElementById('PM') 
 const hourElement = document.getElementById('hours');
 const minutesElement = document.getElementById('minutes');
 const secondsElement = document.getElementById('seconds');
+let hours = 0;
 
-minutesElement.textContent = minutes;
-secondsElement.textContent = seconds;
-hourElement.textContent = hourFormat;
+function updateClock() {
+        
+    const now = new Date();
+    const hourFormat = String(now.getHours()).padStart(2, '0');
+    const minutes = String(now.getMinutes()).padStart(2, '0');
+    const seconds = String(now.getSeconds()).padStart(2, '0');
 
-// let hour = +hourFormat;
-// if(hour > 12){
-//     hour = hour % 12;
-    
-// }
-// else{
-// }
+    minutesElement.textContent = minutes;
+    secondsElement.textContent = seconds;
+    hourElement.textContent = hourFormat;
+
+    const temp = hourElement.textContent;
+    hours = +temp;
+    console.log(hours + ' ' + typeof hours)
+
+    // if hours is less than 12 then reduce opacity of PM else reduce opacity of AM
+    if(hours != 0){
+        isAmPm(hours)
+        
+    }
 }
+    
+function isAmPm(hrs){
+    if(hrs < 12) {
+        pM.style.opacity = '10%'
+        aM.style.opacity = '100%'
+    }
+    else{
+        aM.style.opacity = '10%'
+        pM.style.opacity = '100%'
 
-// Update the clock immediately and then every second
+    }
+}    
+
+    // Update the clock immediately and then every second
 
 updateClock();
 setInterval(updateClock, 1000);
@@ -40,4 +58,6 @@ function changeFormat(){
     toggleBtn.innerHTML = `Change Format to ${format}`;
 }
 toggleBtn.addEventListener('click', changeFormat)
+
+
 
