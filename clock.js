58e497cf4eb99bc1187a897ce0f42a85b7fcb58f
atharva <atharvamk6@gmail.com>
2023-10-21 13:47:50 +0530

    initial commit

---
+++ clock.js
@@ -0,0 +1,20 @@
+
+function updateClock() {
+const now = new Date();
+const hours = String(now.getHours()).padStart(2, '0');
+const minutes = String(now.getMinutes()).padStart(2, '0');
+const seconds = String(now.getSeconds()).padStart(2, '0');
+
+const hoursElement = document.getElementById('hours');
+const minutesElement = document.getElementById('minutes');
+const secondsElement = document.getElementById('seconds');
+
+hoursElement.textContent = hours;
+minutesElement.textContent = minutes;
+secondsElement.textContent = seconds;
+}
+
+// Update the clock immediately and then every second
+
+updateClock();
+setInterval(updateClock, 1000);
